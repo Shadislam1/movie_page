95d8434f449c3e97bb9ede776408f0c9d60ea1d7
shad <shadislam999@gmail.com>
2024-02-21 10:39:56 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Movepage());
+}
+
+class Movepage extends StatelessWidget {
+  const Movepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+  
